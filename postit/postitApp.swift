c4b87c7265ccbff9e663317b478c94e0747df452
Carlos Petit <ca.alberto.p@gmail.com>
2022-11-30 22:37:07 -0500

    Initial Commit

---
+++ postit/postitApp.swift
@@ -0,0 +1,17 @@
+//
+//  postitApp.swift
+//  postit
+//
+//  Created by Carlos Petit on 30-11-22.
+//
+
+import SwiftUI
+
+@main
+struct postitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
